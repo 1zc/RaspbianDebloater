6166ef1720cf71a4522dbd5fac55f71bfe7a5bb8
Liam Z. Charles <liamzcharles@gmail.com>
2019-05-05 12:48:39 +0400

    Raspbian Stretch errors fixed

--- RaspbianDebloater.sh
+++ RaspbianDebloater.sh
@@ -8,6 +8,7 @@
 
 # This script was developed and tested for:
 # RASPBIAN JESSIE --- 2017-07-05 IMAGE (2017-07-05-raspbian-jessie.img)
+# RASPBIAN STRETCH
 # This script will work on all Raspbian images without any FATAL errors.
 # if you encounter any FATAL errors, please create an issue post on the github:
 # https://github.com/LiamZC/RaspbianDebloater
@@ -20,67 +21,33 @@
 #                     -----------------------------------                     #
 
 echo clear
-echo echo Raspbian Debloater v1.2
+echo echo Raspbian Debloater v1.3
 echo echo
-echo "tput bold; tput setaf 2; echo Developed by Liam Z. Charles"
-echo "tput smul; tput setaf 6; echo https://github.com/LiamZC/RaspbianDebloater"
+echo echo Developed by Liam Z. Charles
+echo echo https://github.com/1zc/RaspbianDebloater
 echo echo
-echo "tput rmul; tput bold; tput setaf 1; echo This operation will take a VERY long time."
+echo echo This operation may take a VERY long time.
 echo echo
 
-debloat="
-wolfram-engine
-sense-hat
-scratch nuscratch
-scratch2
-sonic-pi
-minecraft-pi
-python-minecraftpi
-penguinspuzzle
-xpdf
-libreoffice
-libreoffice-base
-libreoffice-base-core
-libreoffice-base-drivers
-libreoffice-calc
-libreoffice-common
-libreoffice-core
-libreoffice-draw
-libreoffice-gtk
-libreoffice-impress
-libreoffice-math
-libreoffice-writer
-claws-mail
-geany-common
-geany
-greenfoot
-bluej
-nodered
-python3-thonny
-sense-emu-tools
-epiphany-browser-data
-epiphany-browser
-dillo
-"
 
-echo "tput bold; tput setaf 3; echo Debloating..."
+echo echo Debloating...
 
-echo sudo apt-get --yes remove --purge wolfram-engine sense-hat scratch nuscratch scratch2 sonic-pi minecraft-pi python-minecraftpi penguinspuzzle xpdf libreoffice libreoffice-base libreoffice-base-core libreoffice-base-drivers
+echo sudo apt-get --yes remove --purge wolfram-engine sense-hat scratch nuscratch scratch2 sonic-pi minecraft-pi python-minecraftpi xpdf libreoffice libreoffice-base libreoffice-base-core libreoffice-base-drivers
 echo sudo apt-get --yes remove --purge libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-writer claws-mail
 echo sudo apt-get --yes remove --purge geany-common geany greenfoot bluej nodered python3-thonny sense-emu-tools epiphany-browser-data epiphany-browser dillo
 
-echo "tput bold; tput setaf 2; echo Done."
+echo echo Done.
 
-echo "tput bold; tput setaf 3; echo Removing unused dependencies..."
+echo echo Removing unused dependencies...
 echo sudo apt-get autoremove -y > /dev/null
-echo "tput bold; tput setaf 2; echo Done."
-echo "tput bold; tput setaf 3; echo Cleaning up..."
+echo echo Done.
+echo echo Cleaning up...
 echo sudo apt-get autoclean -y > /dev/null
-echo "tput bold; tput setaf 2; echo Done."
+echo echo Done.
 
 echo echo
 echo echo
-echo "tput smso; tput setaf 6; echo Debloat process completed successfully."
-echo "tput rmso; tput setaf 7; echo "
+echo echo Debloat process completed successfully.
+echo echo 
 
 
